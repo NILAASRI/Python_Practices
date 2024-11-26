41b15cb2987643ed8446c0ac6772ffc91b3b49d5
NILAASRI M <146331079+NILAASRI@users.noreply.github.com>
2024-11-26 16:03:23 +0530

    Update reverse_string.py

--- Problem_Solving/reverse_string.py
+++ Problem_Solving/reverse_string.py
@@ -8,3 +8,17 @@ def rev_func(string):
 string = str(input("Enter the string:"))
 #print('Original String: ', string)
 print('Reverse String: ', rev_func(string))
+
+
+#Another type for string
+print("----------------Reverse string type-2-----------------")
+a=input("Enter the string:")
+b=a[::-1]
+print(b)
+
+
+
+#Reverse for numbers
+print("---------------Numbers reverse using for loop------------------")
+for i in range(10,0,-1):
+    print(i,end=" ")
