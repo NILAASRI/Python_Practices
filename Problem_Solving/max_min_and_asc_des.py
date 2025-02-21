afc6cab042f97588646ca60b7e83640a2294258b
NILAASRI M <146331079+NILAASRI@users.noreply.github.com>
2025-02-21 14:07:11 +0530

    Add files via upload

---
+++ Problem_Solving/max_min_and_asc_des.py
@@ -0,0 +1,14 @@
+#To find maximum and minimum numbers using max(), min() function
+numbers=[12,23,34,45,56,67,78,89]
+max_value=max(numbers)
+min_value=min(numbers)
+print("Maximum value:",max_value)
+print("Minimum value:",min_value)
+
+
+#Display in ascending and descending order
+num=[23,45,17,56,100,67,78,89,12,34]
+asc_order=sorted(num)
+des_order=sorted(num,reverse=True)
+print("Ascending:",asc_order)
+print("Descending:",des_order)
