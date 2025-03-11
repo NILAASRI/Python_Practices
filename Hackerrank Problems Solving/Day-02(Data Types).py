617983baf77959d5af059c49b69a26786b375794
NILAASRI M <146331079+NILAASRI@users.noreply.github.com>
2025-03-11 21:48:30 +0530

    Add files via upload

---
+++ Hackerrank Problems Solving/Day-02(Data Types).py
@@ -0,0 +1,14 @@
+# Hackerrank Problem 30 Days of Code - (Day 02)
+
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+i1 = int(input())
+d1 = float(input())
+s1 = str(input())
+# Print the sum of the double variables on a new line.
+print(f"{i+i1}\n{d+d1}")
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(s+s1)
