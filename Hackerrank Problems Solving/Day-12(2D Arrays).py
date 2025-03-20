7a9644d45498736c305a793dcaa65352c233ca60
NILAASRI M <146331079+NILAASRI@users.noreply.github.com>
2025-03-20 22:19:25 +0530

    Add files via upload

---
+++ Hackerrank Problems Solving/Day-12(2D Arrays).py
@@ -0,0 +1,65 @@
+# Hackerrank Problem 30 Days of Code - (Day 12)
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__':
+
+    arr = []
+
+    for _ in range(6):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    sums = []
+    for i in range(4):
+        for j in range(4):
+            total = 0
+            for k in range(3):
+                for l in range(3):
+                    if k == 1 and l in (0,2):
+                        continue
+                    total += arr[i+k][j+l]
+            sums.append(total)
+    print(max(sums))
+
+'''
+Sample Input
+
+1 1 1 0 0 0
+0 1 0 0 0 0
+1 1 1 0 0 0
+0 0 2 4 4 0
+0 0 0 2 0 0
+0 0 1 2 4 0
+Sample Output
+
+19
+Explanation
+
+ contains the following hourglasses:
+
+1 1 1   1 1 0   1 0 0   0 0 0
+  1       0       0       0
+1 1 1   1 1 0   1 0 0   0 0 0
+
+0 1 0   1 0 0   0 0 0   0 0 0
+  1       1       0       0
+0 0 2   0 2 4   2 4 4   4 4 0
+
+1 1 1   1 1 0   1 0 0   0 0 0
+  0       2       4       4
+0 0 0   0 0 2   0 2 0   2 0 0
+
+0 0 2   0 2 4   2 4 4   4 4 0
+  0       0       2       0
+0 0 1   0 1 2   1 2 4   2 4 0
+The hourglass with the maximum sum (19) is:
+
+2 4 4
+  2
+1 2 4
+'''
