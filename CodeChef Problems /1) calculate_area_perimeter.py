ade2952ea63ef476eb4509d9af30843938a2cc7e
NILAASRI M <146331079+NILAASRI@users.noreply.github.com>
2025-03-23 19:07:44 +0530

    Update 1) calculate_area_perimeter.py

--- CodeChef Problems /1) calculate_area_perimeter.py
+++ CodeChef Problems /1) calculate_area_perimeter.py
@@ -1,1 +1,20 @@
+# Calculate the Area and Perimeter of Rectangle
+class Rectangle:
+    def __init__(self):
+        self.length = 0
+        self.breadth = 0
+
+    def area(self):
+        print(self.length * self.breadth)
+
+    def perimeter(self):
+        print(2 * (self.length + self.breadth))
+
+r = Rectangle()
+
+r.length = int(input())
+r.breadth = int(input())
+
+r.area()
+r.perimeter()
 
