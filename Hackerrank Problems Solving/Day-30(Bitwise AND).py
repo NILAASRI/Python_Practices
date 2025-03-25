631ad3db87b9a08b0c8463de1301acd42a67444e
NILAASRI M <146331079+NILAASRI@users.noreply.github.com>
2025-03-25 20:47:29 +0530

    Add files via upload

---
+++ Hackerrank Problems Solving/Day-30(Bitwise AND).py
@@ -0,0 +1,64 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'bitwiseAnd' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER N
+#  2. INTEGER K
+#
+
+def bitwiseAnd(N, K):
+    # Write your code here
+    max_possible = K - 1  # The best possible candidate
+    if (max_possible | K) <= N:  
+        return max_possible  # If it can be formed, return it
+    else:
+        return max_possible - 1  # Otherwise, return the next best
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        first_multiple_input = input().rstrip().split()
+
+        count = int(first_multiple_input[0])
+
+        lim = int(first_multiple_input[1])
+
+        res = bitwiseAnd(count, lim)
+
+        fptr.write(str(res) + '\n')
+
+    fptr.close()
+
+'''
+Input (stdin)
+3
+5 2
+8 5
+2 2
+Expected Output
+1
+4
+0
+
+
+
+Sample Input Explanation
+
+STDIN   Function
+-----   --------
+3       T = 3
+5 2     N = 5, K = 2
+8 5     N = 8, K = 5
+2 2     N = 2, K = 2
+'''
