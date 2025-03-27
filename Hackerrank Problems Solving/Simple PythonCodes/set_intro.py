73e637a3741bbf6fffd5a1099ba6a702b4961794
NILAASRI M <146331079+NILAASRI@users.noreply.github.com>
2025-03-27 21:44:18 +0530

    Add files via upload

---
+++ Hackerrank Problems Solving/Simple PythonCodes/set_intro.py
@@ -0,0 +1,19 @@
+def average(array):
+    # your code goes here
+    length = len(set(array))
+    avg = sum(set(array)) / (length)
+    return round(avg,3)
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    result = average(arr)
+    print(result)
+    
+'''
+Input (stdin)
+10
+161 182 161 154 176 170 167 171 170 174
+Your Output (stdout)
+169.375
+'''
