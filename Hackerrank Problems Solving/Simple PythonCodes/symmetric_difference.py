b949f2fca64e458af4e3769fd1396324b872fba9
NILAASRI M <146331079+NILAASRI@users.noreply.github.com>
2025-04-08 19:00:07 +0530

    Add files via upload

---
+++ Hackerrank Problems Solving/Simple PythonCodes/symmetric_difference.py
@@ -0,0 +1,30 @@
+# Read input values
+m = int(input())
+a = set(map(int, input().split())) #converts space-separated input numbers into a set of integers.
+n = int(input())
+b = set(map(int, input().split()))
+
+# Compute symmetric difference
+sym_diff = a.symmetric_difference(b)#a.symmetric_difference(b) or a ^ b: gives the elements in either a or b but not in both.
+
+# Print the result in ascending order
+for num in sorted(sym_diff):
+    print(num)
+
+'''
+Sample Input
+
+STDIN       Function
+-----       --------
+4           set a size M = 4
+2 4 5 9     a = {2, 4, 5, 9}
+4           set b size N = 4
+2 4 11 12   b = {2, 4, 11, 12}
+
+Sample Output
+
+5
+9
+11
+12
+'''
